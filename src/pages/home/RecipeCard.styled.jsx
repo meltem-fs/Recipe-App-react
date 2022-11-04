463a64dec5252a052ff41457669f8b02eb703842
MELTEM <meltemfsanaldi@gmail.com>
2022-11-04 11:17:16 +0300

    styled

---
+++ src/pages/home/RecipeCard.styled.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const RecipeStyled = styled.div`
+  width: 18.74rem;
+  height: 29rem;
+  box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px,
+    rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
+  border-radius: 20px;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export default RecipeStyled;
